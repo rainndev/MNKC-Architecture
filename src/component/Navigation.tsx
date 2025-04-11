783574730f197f88f67080a4883c5e9abf74bd1c
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-12 01:07:29 +0800

    fix(Navigation): fix the icon width and adjust the padding

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,10 +1,10 @@
 const Navigation = () => {
   return (
-    <div className="fixed top-0 left-0 z-10 flex w-full justify-center border-b border-b-[#504F4F] bg-[#1E1E1E]/40 p-2 backdrop-blur-md">
+    <div className="fixed top-0 left-0 z-10 flex w-full justify-center border-b border-b-[#504F4F] bg-[#1E1E1E]/40 p-5 backdrop-blur-md">
       <nav className="flex w-full max-w-7xl items-center justify-between">
         {/* logo */}
         <div>
-          <img src="/images/icon.svg" alt="logo" />
+          <img src="/images/icon-fixed.svg" alt="logo" />
         </div>
         {/* links */}
         <div className="text-[#D9D9D9]">
