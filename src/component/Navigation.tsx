ec003e6f9398d873c53bf894d9c67a0f460cbec8
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-11 23:12:02 +0800

    style: update Navigation component with fixed positioning and color adjustments

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,13 +1,13 @@
 const Navigation = () => {
   return (
-    <div className="fixed top-5 left-0 z-10 flex w-full justify-center">
+    <div className="fixed top-0 left-0 z-10 flex w-full justify-center border-b border-b-[#504F4F] bg-[#1E1E1E]/40 p-2 backdrop-blur-md">
       <nav className="flex w-full max-w-7xl items-center justify-between">
         {/* logo */}
         <div>
           <img src="/images/icon.svg" alt="logo" />
         </div>
         {/* links */}
-        <div className="text-white">
+        <div className="text-[#D9D9D9]">
           <ul className="flex space-x-5 font-sans">
             <li>Home</li>
             <li>About</li>
