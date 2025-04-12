e902d7990e2f1188d50c87352f68dbce8601bab9
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-13 00:44:59 +0800

    fix(Navigation): update z-index for improved layering of navigation component

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -14,7 +14,7 @@ const Navigation = () => {
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -50 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
-      className="fixed top-0 left-0 z-10 flex w-full justify-center border-b border-b-[#504F4F] bg-[#1E1E1E]/40 p-5 backdrop-blur-md"
+      className="fixed top-0 left-0 z-11 flex w-full justify-center border-b border-b-[#504F4F] bg-[#1E1E1E]/40 p-5 backdrop-blur-md"
     >
       <nav className="flex w-full max-w-7xl flex-col items-center justify-between">
         <div className="flex w-full items-center justify-between">
