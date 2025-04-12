61793f99154e116216647afa52d83f4fa8400e9c
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-13 01:51:03 +0800

    fix(config): add server configuration for improved accessibility

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // This allows access from other devices
+    port: 5173, // You can change this port if needed
+  },
 });
