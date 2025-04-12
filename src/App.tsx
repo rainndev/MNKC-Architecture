dddbf0544f6c40bf2151005eb0465f459e954d49
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-12 20:03:06 +0800

    feat(App): integrate Lenis for smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
 import Homepage from "./pages/Homepage";
+import Lenis from "lenis";
 
 const App = () => {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+
+    return () => {
+      lenis.destroy(); // Clean up on unmount
+    };
+  }, []);
+
   return (
     <>
       <div className="flex h-full w-full items-center justify-center bg-[#1E1E1E]">
