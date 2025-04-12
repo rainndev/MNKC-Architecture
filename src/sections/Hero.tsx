4d75b4e1711a286ac6b9ebbd73f9a92dfea303b6
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-13 00:54:17 +0800

    fix(Hero): improve layout and responsiveness of hero section elements

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,14 +6,14 @@ const Hero = () => {
       id="hero"
       className="relative flex w-full items-center justify-center border-b border-[#504F4F] bg-[linear-gradient(to_right,rgba(80,79,79,0.2)_1px,transparent_1px),linear-gradient(to_bottom,rgba(80,79,79,0.1)_1px,transparent_1px)] bg-[size:2rem_2rem]"
     >
-      <div className="grid h-screen w-full max-w-7xl grid-cols-2 items-center justify-center">
-        <div className="w-full">
+      <div className="grid h-screen w-full max-w-7xl grid-cols-1 items-center justify-center p-10 md:grid-cols-2">
+        <div className="z-10 w-full">
           {/* Heading  */}
           <motion.h1
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.1, ease: "easeInOut" }}
-            className="font-[ClashDisplay] text-7xl font-medium text-[#D9D9D9]"
+            className="font-[ClashDisplay] text-7xl text-[clamp(2.5rem,3vw,5rem)] leading-tight font-medium text-pretty text-[#D9D9D9]"
           >
             <span className="text-[#FCAC04]">Designing the Future</span>{" "}
             Building Today
@@ -24,7 +24,7 @@ const Hero = () => {
             initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2, ease: "easeInOut" }}
-            className="mt-4 font-[SansationLight] text-lg font-normal text-[#D9D9D9]"
+            className="mt-4 font-[SansationLight] text-[clamp(1rem,3vw,1.125rem)] font-normal text-[#D9D9D9]"
           >
             Award wining architectural solutions that transform spaces and
             elevate experiences
@@ -37,17 +37,17 @@ const Hero = () => {
             className="mt-10"
           >
             {/* buttons */}
-            <button className="text-md rounded-lg bg-[#D9D9D9]/10 px-6 py-3 font-[ClashDisplay] font-medium text-[#D9D9D9] transition duration-300 ease-in-out hover:bg-[#D9D9D9]/20">
+            <button className="rounded-lg bg-[#D9D9D9]/10 px-6 py-3 font-[ClashDisplay] text-[clamp(.8rem,2vw,1rem)] font-medium text-[#D9D9D9] transition duration-300 ease-in-out hover:bg-[#D9D9D9]/20">
               Start Your Project
             </button>
-            <button className="text-md ml-5 rounded-lg border border-[#504F4F] px-6 py-3 font-[ClashDisplay] font-medium text-white transition duration-300 ease-in-out hover:bg-[#FCAC04] hover:text-[#1E1E1E]">
+            <button className="ml-5 rounded-lg border border-[#504F4F] px-6 py-3 font-[ClashDisplay] text-[clamp(.8rem,2vw,1rem)] font-medium text-white transition duration-300 ease-in-out hover:bg-[#FCAC04] hover:text-[#1E1E1E]">
               Learn More
             </button>
           </motion.div>
         </div>
         <div className="flex h-full w-full items-center"></div>
         <img
-          className="absolute -top-5 right-0 h-full"
+          className="absolute -top-5 right-0 h-full object-cover"
           src="/images/hero-3.png"
           alt=""
         />
