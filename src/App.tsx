a044c2a87f6a020a68268fe3fb1c8bbe48788ae0
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-12 19:57:22 +0800

    feat(App): add background gradient transparent

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import Homepage from "./pages/Homepage";
 
 const App = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center bg-[#1E1E1E]">
-      <Homepage />
-    </div>
+    <>
+      <div className="flex h-full w-full items-center justify-center bg-[#1E1E1E]">
+        <Homepage />
+      </div>
+
+      <div className="fixed right-0 bottom-0 left-0 z-50 flex h-16 w-full bg-gradient-to-t from-[#1E1E1E] to-transparent"></div>
+    </>
   );
 };
 
