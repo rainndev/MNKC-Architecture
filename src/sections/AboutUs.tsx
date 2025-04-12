4b1650ead732608114bcb8f181b87bd111da82c7
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-12 20:26:26 +0800

    feat(AboutUs): enhance heading underline animation for improved visual effect

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -20,7 +20,12 @@ const AboutUs = () => {
               >
                 <span className="text-[#FCAC04]">About</span> us
               </motion.h1>
-              <motion.div className="ml-10 h-[1px] w-full bg-[#504F4F]" />
+              <motion.div
+                initial={{ opacity: 0, width: 0 }}
+                whileInView={{ opacity: 1, width: "100%" }}
+                transition={{ duration: 1, delay: 0.2, ease: "easeInOut" }}
+                className="ml-10 h-[1px] w-full bg-[#504F4F]"
+              />
             </div>
 
             {/* Description */}
