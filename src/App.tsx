06630af9914966aeb4c98c580f85266c7180b7b9
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-13 15:11:32 +0800

    fix: correct favicon path in index.html for accurate display

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
         <Homepage />
       </div>
 
-      <div className="fixed right-0 bottom-0 left-0 z-50 flex h-16 w-full bg-gradient-to-t from-[#1E1E1E] to-transparent"></div>
+      {/* <div className="fixed right-0 bottom-0 left-0 z-50 flex h-16 w-full bg-gradient-to-t from-[#1E1E1E] to-transparent"></div> */}
     </>
   );
 };
