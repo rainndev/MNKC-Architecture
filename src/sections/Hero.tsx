0399ffb52cffabe03925e26e5e8e9bdf96efff56
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-16 17:16:01 +0800

    style: update buttons in hero page, remove the radius to fit the aesthetic

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -45,21 +45,25 @@ const Hero = () => {
             {/* buttons */}
 
             <a href="https://www.facebook.com/MNKCArchitecture" target="_blank">
-              <button className="group relative cursor-pointer overflow-hidden rounded-lg border border-[#504F4F] bg-[#D9D9D9]/10 px-6 py-3 font-[ClashDisplay] text-[clamp(.8rem,2vw,1rem)] font-normal text-white">
-                <span className="relative z-10 transition-colors duration-300 ease-in-out">
-                  Start Your Project
-                </span>
-                <span className="absolute top-0 left-0 z-0 h-full w-0 bg-[#D9D9D9]/20 transition-all duration-500 ease-in-out group-hover:w-full active:w-full" />
-              </button>
+              <div className="inline-block border border-[#504F4F]">
+                <button className="group relative m-[2px] cursor-pointer overflow-hidden bg-[#D9D9D9]/10 px-5 py-2 font-[ClashDisplay] text-[clamp(.8rem,2vw,1rem)] font-normal text-white">
+                  <span className="z-10 transition-colors duration-300 ease-in-out">
+                    Start Your Project
+                  </span>
+                  <span className="absolute top-0 left-0 z-0 h-full w-0 bg-[#D9D9D9]/20 transition-all duration-500 ease-in-out group-hover:w-full active:w-full" />
+                </button>
+              </div>
             </a>
 
             <a href="#about">
-              <button className="group relative mt-2 cursor-pointer overflow-hidden rounded-lg border border-[#504F4F] px-6 py-3 font-[ClashDisplay] text-[clamp(.8rem,2vw,1rem)] font-normal text-white">
-                <span className="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-[#1E1E1E]">
-                  Learn More
-                </span>
-                <span className="absolute top-0 left-0 z-0 h-full w-0 bg-[#FCAC04] transition-all duration-500 ease-in-out group-hover:w-full active:w-full" />
-              </button>
+              <div className="mt-2 inline-block border border-[#504F4F]">
+                <button className="group relative m-[2px] cursor-pointer overflow-hidden border border-[#504F4F] px-5 py-2 font-[ClashDisplay] text-[clamp(.8rem,2vw,1rem)] font-normal text-white">
+                  <span className="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-[#1E1E1E]">
+                    Learn More
+                  </span>
+                  <span className="absolute top-0 left-0 z-0 h-full w-0 bg-[#FCAC04] transition-all duration-500 ease-in-out group-hover:w-full active:w-full" />
+                </button>
+              </div>
             </a>
           </motion.div>
         </div>
