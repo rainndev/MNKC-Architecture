6dbf9c73087f6de1e56b4861ac6175c359fac4ed
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-16 23:30:14 +0800

    feat: enhance styling and hover effects in CardsTeam component

--- src/components/CardsTeam.tsx
+++ src/components/CardsTeam.tsx
@@ -9,19 +9,23 @@ interface CardsTeamProps {
 const CardsTeam = ({ data }: CardsTeamProps) => {
   const { name, position, image } = data;
   return (
-    <div className="z-50">
+    <div className="group">
       <div className="h-96 w-72 overflow-hidden border border-[#504F4F] bg-white/10">
         <img
           loading="lazy"
           src={image}
-          className="h-full w-full object-cover opacity-20 transition-all duration-300 ease-in-out hover:scale-103 hover:opacity-100"
+          className="h-full w-full object-cover opacity-20 transition-all duration-300 ease-in-out group-hover:scale-103 hover:opacity-100"
         />
       </div>
 
       {/* Info */}
       <div className="mt-2">
-        <h4 className="text-[clamp(.9rem,3vw,1.3rem)]">{name}</h4>
-        <p className="font-[SansationLight] text-white/60">{position}</p>
+        <h4 className="font-[ClashDisplay] text-[clamp(.9rem,3vw,1.3rem)] text-[#D9D9D9] transition-all duration-300 ease-in-out group-hover:text-2xl">
+          {name}
+        </h4>
+        <p className="font-[SansationLight] text-sm text-white/60 transition-all duration-300 ease-in-out group-hover:text-base">
+          {position}
+        </p>
       </div>
     </div>
   );
