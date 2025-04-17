cf797d2e2d731161cdc93c38d470e365979f0cff
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-17 18:31:51 +0800

    feat: add LeftSideOverlay component for enhanced team data display

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
 import Homepage from "@/pages/Homepage";
-import { AnimatePresence, motion } from "framer-motion";
 import Lenis from "lenis";
-import { useLeftSideOverlayStore } from "@/store/LeftSideOverlayStore";
 import { Routes, Route } from "react-router-dom";
 import Teamspage from "@/pages/Teamspage";
 import Navigation from "@/components/Navigation";
+import LeftSideOverlay from "./components/LeftSideOverlay";
 
 const App = () => {
-  const leftSidedata = useLeftSideOverlayStore((state) => state.data);
   useEffect(() => {
     // Initialize Lenis
     const lenis = new Lenis({
@@ -29,33 +27,10 @@ const App = () => {
           <Route path="/" element={<Homepage />} />
           <Route path="/teams" element={<Teamspage />} />
         </Routes>
-        <div className="z-50 hidden md:bottom-10 lg:fixed lg:left-10 lg:block lg:w-[20%]">
-          <AnimatePresence mode="wait">
-            {leftSidedata.number !== "00" && (
-              <motion.div
-                key={leftSidedata.number}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.3, ease: "easeInOut" }}
-                className="pointer-events-none flex origin-bottom-left -rotate-90 items-center space-x-5 font-[ClashDisplay] text-sm font-light tracking-widest text-white"
-              >
-                <p className="whitespace-nowrap">{leftSidedata.number}</p>
-                <motion.div
-                  initial={{ opacity: 0, width: 0 }}
-                  animate={{ opacity: 1, width: "100%" }}
-                  exit={{ opacity: 0, width: 0 }}
-                  transition={{ duration: 0.5, ease: "easeInOut" }}
-                  className="h-[1px] w-15 bg-[#FCAC04]"
-                />
-                <p className="text-nowrap">{leftSidedata.title}</p>
-              </motion.div>
-            )}
-          </AnimatePresence>
-        </div>
-      </div>
 
-      {/* <div className="fixed right-0 bottom-0 left-0 z-50 flex h-16 w-full bg-gradient-to-t from-[#1E1E1E] to-transparent"></div> */}
+        {/* Left side overlay */}
+        <LeftSideOverlay />
+      </div>
     </>
   );
 };
---
+++ src/components/LeftSideOverlay.tsx
@@ -0,0 +1,36 @@
+import { useLeftSideOverlayStore } from "@/store/LeftSideOverlayStore";
+import { AnimatePresence, motion } from "framer-motion";
+
+const LeftSideOverlay = () => {
+  const leftSidedata = useLeftSideOverlayStore((state) => state.data);
+  return (
+    <div>
+      <div className="z-50 hidden md:bottom-10 lg:fixed lg:left-10 lg:block lg:w-[20%]">
+        <AnimatePresence mode="wait">
+          {leftSidedata.number !== "00" && (
+            <motion.div
+              key={leftSidedata.number}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="pointer-events-none flex origin-bottom-left -rotate-90 items-center space-x-5 font-[ClashDisplay] text-sm font-light tracking-widest text-white"
+            >
+              <p className="whitespace-nowrap">{leftSidedata.number}</p>
+              <motion.div
+                initial={{ opacity: 0, width: 0 }}
+                animate={{ opacity: 1, width: "100%" }}
+                exit={{ opacity: 0, width: 0 }}
+                transition={{ duration: 0.5, ease: "easeInOut" }}
+                className="h-[1px] w-15 bg-[#FCAC04]"
+              />
+              <p className="text-nowrap">{leftSidedata.title}</p>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideOverlay;
