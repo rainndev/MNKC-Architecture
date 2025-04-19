23095b14e10197a0a7a5b5fd9f360f7a44b45189
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-19 12:43:27 +0800

    style: enhance layout and typography on Thank You page for improved user experience

--- src/pages/ThankYouPage.tsx
+++ src/pages/ThankYouPage.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 const ThankYouPage = () => {
   return (
     <div className="flex h-screen w-full items-center justify-center bg-[#1E1E1E]">
-      <div className="bg-striped flex flex-col items-center justify-center gap-5 border-x-5 border-[#504F4F]/20 p-10 text-center md:p-20">
-        <h1 className="font-[ClashDisplay] text-[clamp(1.5rem,4vw,4rem)] font-medium text-pretty text-[#FCAC04]">
+      <div className="bg-striped flex max-w-4xl flex-col items-center justify-center gap-5 border-x-5 border-[#504F4F]/20 p-5 text-center md:p-20">
+        <h1 className="font-[ClashDisplay] text-[clamp(1.5rem,3vw,5rem)] font-medium text-pretty text-[#FCAC04]">
           Thanks for the message!
         </h1>
-        <p className="font-[SansationLight] text-[clamp(.8rem,3vw,1.125rem)] font-normal text-[#D9D9D9]">
-          Thank you for reaching out. We’ll be in touch as soon as possible.
+        <p className="font-[SansationLight] text-[clamp(.8rem,2vw,1.125rem)] font-normal text-[#D9D9D9]">
+          Thank you for reaching out. We’ll be in touch as soon as possible. In
+          the meantime, feel free to explore our website and learn
         </p>
         <Link to={"/"}>
           <div className="group inline-block border border-[#504F4F]">
