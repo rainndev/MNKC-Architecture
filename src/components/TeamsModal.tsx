4ffa10a768f15debaa9df23999f426d360c6427f
rainndev <182114243+rainndev@users.noreply.github.com>
2025-04-24 17:35:44 +0800

    style: enhance animations in TeamsModal for improved user experience

--- src/components/TeamsModal.tsx
+++ src/components/TeamsModal.tsx
@@ -21,7 +21,13 @@ const TeamsModal = () => {
             >
               <MdClose />
             </div>
-            <div className="m-5 w-fit max-w-4xl items-center justify-center">
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+              transition={{ duration: 0.5, ease: "easeInOut" }}
+              className="m-5 w-fit max-w-4xl items-center justify-center"
+            >
               <div className="bg-striped border-logo-gray/50 grid h-full w-full grid-cols-1 items-center justify-center border border-dashed md:grid-cols-2">
                 <div>
                   <img
@@ -35,36 +41,64 @@ const TeamsModal = () => {
                 {/* Profile info */}
                 <div className="item text-logo-white flex h-full w-full flex-col items-start justify-center p-7 md:p-10">
                   {/* Job title */}
-                  <h1 className="font-[ClashDisplay] text-[clamp(1.8rem,2vw,2.25rem)] font-normal">
+                  <motion.h1
+                    initial={{ opacity: 0, x: -10 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    exit={{ opacity: 0, x: -20 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                    className="font-[ClashDisplay] text-[clamp(1.8rem,2vw,2.25rem)] font-normal"
+                  >
                     {position}
-                  </h1>
+                  </motion.h1>
 
                   {/* expertise/skills */}
                   <div className="space-x-2">
                     {expertise.map((skills, i) => (
-                      <span
+                      <motion.span
+                        initial={{ opacity: 0, x: -10 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -20 }}
+                        transition={{
+                          duration: 0.5,
+                          delay: i * 0.1,
+                          ease: "easeInOut",
+                        }}
                         key={i}
-                        className="mt-2 inline-block rounded-full bg-white/20 px-3 py-1 text-[clamp(.75rem,2vw,.9rem)]"
+                        className="border-logo-white/30 mt-2 inline-block border bg-white/20 px-3 py-1 text-[clamp(.75rem,2vw,.9rem)]"
                       >
                         {skills}
-                      </span>
+                      </motion.span>
                     ))}
                   </div>
 
                   {/* short bio */}
 
-                  <p className="mt-5 text-[clamp(.875rem,2vw,1.2rem)]">{bio}</p>
+                  <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                    className="mt-5 text-[clamp(.875rem,2vw,1.2rem)]"
+                  >
+                    {bio}
+                  </motion.p>
 
                   {/* LinkedIn button */}
-                  <div className="mt-3 flex items-center text-2xl">
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.5, ease: "easeInOut" }}
+                    className="mt-3 flex items-center text-2xl"
+                  >
                     <FaLinkedin />
                     <a href={linkedIn} className="ml-2 text-sm">
                       LinkedIn
                     </a>
-                  </div>
+                  </motion.div>
                 </div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </motion.div>
       )}
