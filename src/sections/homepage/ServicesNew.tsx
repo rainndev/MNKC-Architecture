0d5cd00b7f649768ac3bf783f00a02eb4b02f3d8
rainndev <182114243+rainndev@users.noreply.github.com>
2025-05-04 09:45:47 +0800

    style(services): add circle background blur

--- src/sections/homepage/ServicesNew.tsx
+++ src/sections/homepage/ServicesNew.tsx
@@ -8,9 +8,11 @@ const ServicesNew = () => {
   return (
     <section
       ref={ref}
-      className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-center p-7"
+      className="relative flex min-h-screen w-full flex-col items-center justify-center bg-[linear-gradient(to_right,rgba(80,79,79,0.4)_1px,transparent_1px),linear-gradient(to_bottom,rgba(80,79,79,0.4)_1px,transparent_1px)] bg-[size:2rem_2rem] p-7"
     >
-      <div className="flex h-full w-full flex-col items-center justify-center text-center font-[ClashDisplay]">
+      <div className="bg-logo-black pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="md:bg-logo-yellow/60 absolute z-0 hidden h-[500px] w-[600px] rounded-full blur-[120px] sm:block" />
+      <div className="z-10 flex h-full w-full flex-col items-center justify-center text-center font-[ClashDisplay]">
         <motion.p
           initial={{ opacity: 0, y: -30 }}
           animate={{ opacity: 1, y: 0 }}
@@ -29,7 +31,7 @@ const ServicesNew = () => {
         </motion.h1>
       </div>
 
-      <div className="mt-15 grid h-full grid-cols-1 items-center justify-center gap-4 md:grid-cols-2 md:gap-5 lg:grid-cols-3">
+      <div className="z-10 mt-15 grid h-full max-w-7xl grid-cols-1 items-center justify-center gap-4 md:grid-cols-2 md:gap-5 lg:grid-cols-3">
         {content.map((data, i) => (
           <motion.div
             key={i}
@@ -37,10 +39,10 @@ const ServicesNew = () => {
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, ease: "easeInOut", delay: i * 0.1 }}
             viewport={{ once: true }}
-            className="text-logo-white bg-logo-black h-full w-full"
+            className="text-logo-white h-full w-full"
           >
             <div className="from-logo-white/10 via-logo-gray/50 to-logo-yellow/30 h-full rounded-lg bg-gradient-to-r p-[2px] md:rounded-xl">
-              <div className="bg-logo-black hover:bg-logo-black/80 hover:border-logo-yellow/10 border-logo-gray/30 h-full rounded-lg border p-5 text-white transition-all duration-500 ease-in-out hover:backdrop-blur-2xl md:rounded-xl md:p-10">
+              <div className="bg-logo-black/90 md:bg-logo-black/80 hover:bg-logo-black/50 hover:border-logo-yellow/10 border-logo-gray/30 h-full rounded-lg border p-5 text-white transition-all duration-500 ease-in-out hover:backdrop-blur-2xl md:rounded-xl md:p-10">
                 <div className="flex items-center justify-start gap-4">
                   <span className="text-logo-yellow bg-logo-gray/20 bg-striped rounded-full p-3">
                     {data.icon}
